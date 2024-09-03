063f2c42d452fe5da79dc29d75c8ce6097e17a3c
Yahya Rouissi <yahyarouissi@gmail.com>
2024-09-03 13:23:49 +0300

    Lisätty komponentti

---
+++ src/components/Notification.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+
+export const Notification = ({ message }) => {
+  if (!message) {
+    return null; // Do not render anything if there is no message
+  }
+  const notificationStyle = {
+    border: "solid",
+    padding: 10,
+    borderWidth: 1,
+    marginBottom: 10,
+    color: "green",
+  };
+  return <div style={notificationStyle}>{message}</div>;
+};
+
+Notification.propTypes = {
+  message: PropTypes.string,
+};
