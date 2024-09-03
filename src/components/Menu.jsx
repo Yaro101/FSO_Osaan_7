3c18b1e611f6999859b6fb7305799883101e9d85
Yahya Rouissi <yahyarouissi@gmail.com>
2024-09-03 14:14:45 +0300

    Lisätty tyyli

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,10 +2,14 @@ import { Link } from "react-router-dom";
 
 export const Menu = () => {
     const padding = {
-      paddingRight: 5,
+      paddingRight: 20,
     };
+    const menuStyle = {
+        backgroundColor: 'whitesmoke',
+        padding: '16px',
+    }
     return (
-      <div>
+      <div style={menuStyle}>
         <Link to="/" style={padding}>
           anecdotes
         </Link>
