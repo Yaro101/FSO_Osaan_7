79f2e72486c27d0da08ed9b4cf941f0cdc2e807a
Yahya Rouissi <yahyarouissi@gmail.com>
2024-09-03 13:13:54 +0300

    Lisätty <br/> alkuun

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 export const Footer = () => {
   return (
     <div>
+      <br />
       Anecdote app for <a href="https://fullstackopen.com/">Full Stack Open</a>.
       See{" "}
       <a href="https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js">
